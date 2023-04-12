ee4a2817098281856d61b8f756408090fe6940f1
you@example.com <you@example.com>
2023-04-12 16:10:41 -0600

    Documentcion con swagger

--- src/routes/characters_routes.js
+++ src/routes/characters_routes.js
@@ -2,10 +2,234 @@ import Router from 'express';
 const router = Router();
 import { getAllCharacters, findCharacter, updateCharacter, deleteCharacter, createCharacter } from '../controllers/charactersController.js';
 
+/**
+ * @swagger
+ * /api/characters/getAllCharacters:
+ * 
+ *   get:
+ *     description: "Fetches lists of comic characters"
+ *     tags:
+ *       - Characters
+ *     responses:
+ *       200:
+ *         description: "Successful operation"
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array 
+ *                   items: 
+ *                     type: object
+ *       5XX:
+ *         description: FAILED
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status: 
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string 
+ *                       example: "Some error message"
+*/
 router.get('/getAllCharacters', getAllCharacters);
+/**
+ * @swagger
+ * /api/characters/findCharacter/{id}:
+ * 
+ *   get:
+ *     description: "Finds a single comic  character by entityId"
+ *     tags:
+ *       - Characters
+ *     produces:
+ *          -application/json
+ *     parameters:
+ *          - in: path
+ *            name: id
+ *            description:  EntityId of comic character
+ *     responses:
+ *       200:
+ *         description: "Successful operation"
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array 
+ *                   items: 
+ *                     type: object
+*/
+
 router.get('/findCharacter/:id', findCharacter);
+/**
+ * @swagger
+ * /api/characters/updateCharacter:
+ * 
+ *   patch:
+ *     description: "Lets you update a character now his entityId"
+ *     tags:
+ *       - Characters
+ *     produces:
+ *          -application/json
+ *     consumes:
+ *      - application/json
+ *     parameters:
+ *      - in: body
+ *        name: user
+ *        description: The user to create.
+ *        schema:
+ *          type: object
+ *          required:
+ *            - entityId
+ *            - name
+ *          properties:
+ *            entityId:
+ *             type: string
+ *            name:
+ *              type: string
+ *            description:
+ *              type: string
+ *     responses:
+ *       200:
+ *         description: "Successful operation"
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array 
+ *                   items: 
+ *                     type: object
+ *       409:
+ *         description: Invalid or unrecognized parameter.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status: 
+ *                   type: string
+ *                   example: FAILED
+*/
+
 router.patch('/updateCharacter', updateCharacter);
+
+
+/**
+ * @swagger
+ * /api/characters/deleteCharacter/{id}:
+ * 
+ *   delete:
+ *     description: "Lets you delete a character now his entityId"
+ *     tags:
+ *       - Characters
+ *     produces:
+ *          -application/json
+ *     consumes:
+ *      - application/json
+ *     parameters:
+ *      - in: path
+ *        name: id
+ *        description: The user to create.
+ *     responses:
+ *       200:
+ *         description: "Successful operation"
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array 
+ *                   items: 
+ *                     type: object
+ *       409:
+ *         description: Invalid or unrecognized parameter.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status: 
+ *                   type: string
+ *                   example: FAILED
+*/
+
 router.delete('/deleteCharacter/:id', deleteCharacter);
+
+/**
+ * @swagger
+ * /api/characters/createCharacter:
+ * 
+ *   post:
+ *     description: "Lets you create a character"
+ *     tags:
+ *       - Characters
+ *     produces:
+ *          -application/json
+ *     consumes:
+ *      - application/json
+ *     parameters:
+ *      - in: body
+ *        name: user
+ *        description: The user to create.
+ *        schema:
+ *          type: object
+ *          required:
+ *            - name
+ *          properties:
+ *            name:
+ *              type: string
+ *            description:
+ *              type: string
+ *     responses:
+ *       200:
+ *         description: "Successful operation"
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array 
+ *                   items: 
+ *                     type: object
+ *       409:
+ *         description: Invalid or unrecognized parameter.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status: 
+ *                   type: string
+ *                   example: FAILED
+*/
+
 router.post("/createCharacter", createCharacter);
 
 export const characterRouteList = router;
--- src/server.js
+++ src/server.js
@@ -3,11 +3,37 @@ import 'dotenv/config';
 import express from 'express';
 import { characterRepository } from './models/character.js';
 import { getAllCharactersOnApiServices } from './services/charactersServices.js';
-import {characterRouteList} from './routes/characters_routes.js';
+import { characterRouteList } from './routes/characters_routes.js';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
 
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      version: "1.0.0",
+      title: "SPS Marvel Api tech test",
+      description: "This is a sample server Marvel Characterstore serve using redis as DB",
+      contact: {
+        name: "Bryan Eliut Hernandez Moran",
+        url: "https://bhdzit.github.io/"
+      },
+      servers: ["localhost:3000"]
+    },
+  },
+
+  apis: ["./src/routes/characters_routes.js"]
+};
+
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+
+
+
 app.use(express.json());
 app.use("/api/characters", characterRouteList);
 
@@ -36,3 +62,4 @@ app.listen(port, async () => {
 
 
 });
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
